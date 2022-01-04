dbf778708983709431df651936e611ffb2e64cee
Ghilman <ghilmanrandhawa@gmail.com>
2022-01-04 16:22:59 +0500

    set urls

--- banks/v3/urls.py
+++ banks/v3/urls.py
@@ -5,7 +5,9 @@ from banks.v3 import views
 urlpatterns = [
     path('banks/', views.BankListCreateAPI.as_view(), name='bank_listing'),
     path('bank-details/<int:pk>/', views.BankUpdateDeleteApi.as_view(), name='bank_details'),
+    path('accounts/', views.AccountListCreateAPI.as_view(), name='account_details'),
     path('account-details/<int:pk>/', views.AccountUpdateDeleteAPI.as_view(), name='account_details'),
     path('transactions/', views.TransactionsListCreateApi.as_view(), name='transaction_list'),
+    path('transaction-details/<int:pk>', views.TransactionDeleteAPI.as_view(), name='transaction_details'),
 
     ]
